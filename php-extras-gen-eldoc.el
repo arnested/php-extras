ffb56cc983f5124fd714ae42d49efefaaa0cbc35
Arne Jørgensen <arne@arnested.dk>
2013-09-07 21:28:35 +0200

    Rewritten to use funcsummary.txt

--- php-extras-gen-eldoc.el
+++ php-extras-gen-eldoc.el
@@ -27,41 +27,16 @@
 ;; Please note that build a new `php-extras-function-arguments' is a
 ;; slow process and might be error prone.
 
-;; The code in this file can must likely be heavily improved.
-
 ;;; Code:
 
 (require 'php-extras)
 
 
 
-(defvar php-extras-gen-eldoc-temp-methodname nil)
-
-(defvar php-extras-php-manual-url "http://dk1.php.net/distributions/manual/php_manual_en.tar.gz"
-  "URL of the downloadable .tar.gz version of the PHP manual.")
+(defvar php-extras-php-funcsummary-url "http://svn.php.net/repository/phpdoc/doc-base/trunk/funcsummary.txt"
+  "URL of the PHP funcsummary.txt file.")
 
 
-
-(defun php-extras-gen-eldoc-elem-string (elem)
-  "Helper function for regenerating PHP function argument hash."
-  ;; Element is a string it self just return it.
-  (if (stringp elem)
-      elem
-    ;; If element is a list of exactly 3 elements - we continue with
-    ;; the 3rd element.
-    (if (and (listp elem) 
-             (= (safe-length elem) 3))
-        (progn
-          (ignore-errors
-            (when (string= (cdr (car (car (cdr elem)))) "methodname")
-              (setq php-extras-gen-eldoc-temp-methodname (nth 2 (nth 2 elem)))))
-          (php-extras-gen-eldoc-elem-string (nth 2 elem)))
-      ;; If the element is a list with anything but 3 elements iterate
-      ;; over them.
-      (if (listp elem)
-          (let ((result ""))
-            (dolist (elem2 (cdr (cdr elem)) result)
-              (setq result (concat result (php-extras-gen-eldoc-elem-string elem2)))))))))
 
 ;;;###autoload
 (defun php-extras-generate-eldoc ()
@@ -73,37 +48,22 @@
 (defun php-extras-generate-eldoc-1 (&optional byte-compile)
   "Regenerate PHP function argument hash table from php.net. This is slow!"
   (save-excursion
-    (let ((gzip (executable-find "gzip"))
-          (php-extras-function-arguments (make-hash-table
-                                          :size 4400
+    (let ((php-extras-function-arguments (make-hash-table
+                                          :size 8400
                                           :rehash-threshold 1.0
-                                          :rehash-size 100
+                                          :rehash-size 1.1
                                           :test 'equal))
-          (tar-buffer nil))
-      (with-temp-buffer (url-insert-file-contents php-extras-php-manual-url)
-                        (call-process-region (point-min) (point-max) gzip t t nil "-c" "-d")
-                        (tar-mode)
-                        (setq tar-buffer (current-buffer))
+          (methodname nil)
+          (help-string ""))
+      (with-temp-buffer (url-insert-file-contents php-extras-php-funcsummary-url)
                         (goto-char (point-min))
-                        (while (re-search-forward "/function\.\\(.+\\)\.html" nil t)
-                          (message "Parsing %s..." (match-string-no-properties 1))
-                          (let ((buf (tar-extract)))
-                            (with-current-buffer buf
-                              (goto-char (point-min))
-                              (let ((min nil)
-                                    (max nil)
-                                    (help-string ""))
-                                (when (search-forward "<div class=\"methodsynopsis dc-description\">" nil t)
-                                  (setq min (point))
-                                  (search-forward "</div>" nil t)
-                                  (setq max (point))
-                                  (dolist (elem (cdr (cdr (nth 2 (libxml-parse-html-region min max)))))
-                                    (setq help-string (concat help-string (php-extras-gen-eldoc-elem-string elem))))
-                                  (setq help-string (replace-regexp-in-string "[ \n]+" " " help-string))
-                                  (when (and php-extras-gen-eldoc-temp-methodname help-string)
-                                    (puthash php-extras-gen-eldoc-temp-methodname help-string php-extras-function-arguments)))))
-                            (kill-buffer buf))
-                          (set-buffer tar-buffer)))
+                        (while (re-search-forward "^[^(]* \\([^(]+\\)(.*)
+ +.*" nil t)
+                          (setq methodname (match-string-no-properties 1))
+                          (message "Parsing %s..." methodname)
+                          (setq help-string (replace-regexp-in-string "[ \t]+" " " (replace-regexp-in-string "
+" " - " (match-string-no-properties 0))))
+                          (puthash methodname help-string php-extras-function-arguments)))
       (let* ((file (concat php-extras-eldoc-functions-file ".el"))
              (buf (find-file file)))
         (with-current-buffer buf
